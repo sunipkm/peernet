fd9cabd759d23e8e6f5ea9fb15c9d9b5b59b2a97
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2022-10-31 13:41:37 -0400

    Updated README. Changed build_win chat example target name. Finalized v3.0.0.

--- build_win.bat
+++ build_win.bat
@@ -7,7 +7,7 @@
 mkdir %OUT_DIR%
 cl /GS /GL /analyze- /W3 /Gy /Zc:wchar_t /Zi /Gm- /O2 /Zc:inline /fp:precise /WX- /Zc:forScope /Gd /Oy- /Oi /nologo /Zi /MD /EHsc /wd4005 /DWIN32 /D__WINDOWS__ %INCLUDES% /D UNICODE /D _UNICODE /D_USRDLL /D_WINDLL %SOURCES% /Fo%OUT_DIR%/ /link %LIBS% /DLL /OUT:%OUT_DIR%/%OUT_EXE%
 
-@set OUT_EXE=chat.exe
+@set OUT_EXE=peerchat.exe
 @set SOURCES=examples\chat\chat.c
 @set LIBS=/LIBPATH:Release libpeer.lib /LIBPATH:C:\libzmq\lib zyre.lib czmq.lib libzmq-v142-mt-4_3_4.lib libsodium.lib
 cl /GS /GL /analyze- /W3 /Gy /Zc:wchar_t /Zi /Gm- /O2 /Zc:inline /fp:precise /WX- /Zc:forScope /Gd /Oy- /Oi /nologo /Zi /MD /EHsc /wd4005 /DWIN32 /D__WINDOWS__ %INCLUDES% /D UNICODE /D _UNICODE %SOURCES% /Fe%OUT_DIR%/%OUT_EXE% /Fo%OUT_DIR% /link %LIBS%
