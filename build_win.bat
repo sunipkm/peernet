7aa1797009b1c1ad1ad41e7f3bb671827b157b83
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2022-10-31 14:13:01 -0400

    Updating build system for windows

--- build_win.bat
+++ build_win.bat
@@ -6,8 +6,12 @@
 @set LIBS=/LIBPATH:C:\libzmq\lib zyre.lib czmq.lib libzmq-v142-mt-4_3_4.lib libsodium.lib
 mkdir %OUT_DIR%
 cl /GS /GL /analyze- /W3 /Gy /Zc:wchar_t /Zi /Gm- /O2 /Zc:inline /fp:precise /WX- /Zc:forScope /Gd /Oy- /Oi /nologo /Zi /MD /EHsc /wd4005 /DWIN32 /D__WINDOWS__ %INCLUDES% /D UNICODE /D _UNICODE /D_USRDLL /D_WINDLL %SOURCES% /Fo%OUT_DIR%/ /link %LIBS% /DLL /OUT:%OUT_DIR%/%OUT_EXE%
+copy include\*.h C:\libzmq\include\
+copy Release\libpeer.dll C:\libzmq\bin\
+copy Release\libpeer.lib C:\libzmq\lib\
 
 @set OUT_EXE=peerchat.exe
 @set SOURCES=examples\chat\chat.c
 @set LIBS=/LIBPATH:Release libpeer.lib /LIBPATH:C:\libzmq\lib zyre.lib czmq.lib libzmq-v142-mt-4_3_4.lib libsodium.lib
 cl /GS /GL /analyze- /W3 /Gy /Zc:wchar_t /Zi /Gm- /O2 /Zc:inline /fp:precise /WX- /Zc:forScope /Gd /Oy- /Oi /nologo /Zi /MD /EHsc /wd4005 /DWIN32 /D__WINDOWS__ %INCLUDES% /D UNICODE /D _UNICODE %SOURCES% /Fe%OUT_DIR%/%OUT_EXE% /Fo%OUT_DIR% /link %LIBS%
+copy Release\peerchat.exe C:\libzmq\bin\
